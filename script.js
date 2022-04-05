1bead0402871d266328dadc4e5128df3f62a0fc6
juliet@21 <julietyartes2@gmail.com>
2022-04-05 10:50:54 -0500

    termina

---
+++ script.js
@@ -0,0 +1,18 @@
+let promedio=prompt('55% promedio de sus tres calificaciones parciales:')
+let examenFinal=prompt('30% de la calificacion del examen final')
+let trabajoFinal=prompt('15% de la calicacion de un trabajo final')
+let total;
+
+
+
+total=promedio/100*55
+total=examenFinal/100*30
+total=trabajoFinal/100*15
+
+document.write("la nota final : "+total)
+
+
+
+
+
+
